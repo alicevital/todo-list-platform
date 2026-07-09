ae3c798604c047673a94b7a698960ab9d031aacc
Alice Nascimento <alicevnascimento02@gmail.com>
2026-07-09 16:21:57 -0300

    feat: create custom user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+
+    "modelo de usuário personalizado"
+    "utiliza os campos do abstract user mas pode ser personalizado"
+    "futuramente"
+
+    pass
+
 # Create your models here.
