9871bd4a0a2793e2dacc9ce0d530cc0473c7f633
Alice Nascimento <alicevnascimento02@gmail.com>
2026-07-09 16:36:29 -0300

    feat: implements user register

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .models import User
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
+
